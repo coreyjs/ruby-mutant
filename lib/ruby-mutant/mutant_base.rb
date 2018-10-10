12567404e2cb74694dfb79d6ea741212fa2f2669
Corey Schaf <coreyschaf@Coreys-iMac.verizon.net>
2018-10-09 21:14:03 -0400

    early laying down of code foundation

--- lib/ruby-mutant/mutant_base.rb
+++ lib/ruby-mutant/mutant_base.rb
@@ -1,4 +1,13 @@
 module Mutant
     class MutantBase
+        def self.run(*args)
+            puts 'parent'
+            puts args
+            # 
+            
+            # TODO Check for all validator
+            # Run all validators, checking errors
+
+        end
     end
 end
--- spec/dummy/product.rb
+++ spec/dummy/product.rb
@@ -1,3 +1,31 @@
 require 'mutant'
 
 puts 'hello world'
+
+class ProductCreatedMutation < Mutant::MutantBase
+
+    def validate_name
+        true
+    end
+
+    # execute out mutation code that is
+    # specific to ProductCreatedMutation
+    def self.run(product)
+        super
+        puts 'child'
+        puts product
+        # Do other logic
+    end
+end
+
+class Product
+
+    def initialize()
+        @name = 'Beer'
+        ProductCreatedMutation.run(product: self)
+    end
+end
+
+
+
+p = Product.new
