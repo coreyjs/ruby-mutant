92cc1333446acff7f71905e53bacd240e341fce9
Corey Schaf <coreyschaf@Coreys-iMac.verizon.net>
2018-10-10 23:17:43 -0400

    Base mutation class laid out.  Setting up logical structure

--- lib/ruby-mutant/mutant_base.rb
+++ lib/ruby-mutant/mutant_base.rb
@@ -1,13 +1,54 @@
 module Mutant
     class MutantBase
-        def self.run(*args)
-            puts 'parent'
-            puts args
-            # 
-            
-            # TODO Check for all validator
-            # Run all validators, checking errors
+        @props
+        @errors
 
+        def success?
+            !@errors
+        end
+
+        def self.run(*args)
+            puts 'base.run'
+
+            @props.each do |p|
+                present = args.any?{|arg| arg.key? p.first}
+                if !present
+                    #TODO Add error
+                end
+            end
+        end
+
+        def self.set_attributes(param_type, &block)
+            fields = yield block
+            self.props
+            fields.each do |k, klass|
+                #TODO Check for dup, throw mutation error
+                @props[k] = klass
+            end
+        end
+
+        def self.props
+            @props ||= begin
+                if Base == self.superclass
+                    {}
+                else
+                    self.superclass.props.dup
+                end
+            end
+        end
+
+        # validate our required props equals whats inputed
+        def self.validate_props(*args)
+
+        end
+
+        def self.required(&block)
+            self.set_attributes('required', &block)
+        end
+
+        def self.optional(*block)
+            #TODO Add to props
+            # check for dups, throw mutation error
         end
     end
 end
--- spec/dummy/product.rb
+++ spec/dummy/product.rb
@@ -2,9 +2,79 @@ require 'mutant'
 
 puts 'hello world'
 
-class ProductCreatedMutation < Mutant::MutantBase
+class Base
+    @props
+    @errors
 
-    def validate_name
+    def success?
+        !@errors
+    end
+
+    def self.run(*args)
+        puts 'base.run'
+
+        @props.each do |p|
+            present = args.any?{|arg| arg.key? p.first}
+            if !present
+                #TODO Add error
+            end
+        end
+    end
+
+    def self.set_attributes(param_type, &block)
+        fields = yield block
+        self.props
+        fields.each do |k, klass|
+            #TODO Check for dup, throw mutation error
+            @props[k] = klass
+        end
+
+    end
+
+    def self.props
+        @props ||= begin
+            if Base == self.superclass
+                {}
+            else
+                self.superclass.props.dup
+            end
+        end
+    end
+
+    # validate our required props equals whats inputed
+    def self.validate_props(*args)
+
+    end
+
+    def self.required(&block)
+        self.set_attributes('required', &block)
+    end
+
+    def self.optional(*block)
+        #TODO Add to props
+        # check for dups, throw mutation error
+    end
+
+end
+
+class Product
+
+    def initialize()
+        @name = 'Beer'    
+    end
+end
+
+class ProductCreatedMutation < Base
+
+    required do
+        {
+            name: String, 
+            address: String, 
+            product: Product
+        }
+    end
+
+    def validate_name?
         true
     end
 
@@ -18,14 +88,9 @@ class ProductCreatedMutation < Mutant::MutantBase
     end
 end
 
-class Product
 
-    def initialize()
-        @name = 'Beer'
-        ProductCreatedMutation.run(product: self)
-    end
-end
 
 
 
 p = Product.new
+ProductCreatedMutation.run(product: p)
